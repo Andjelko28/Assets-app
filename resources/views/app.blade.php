a88669a3ff5c0e8849d72726288dace1c08cca5e
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-06-20 08:15:58 +0200

    "Updated app.blade.php: added Google Fonts links, modified header and main layout with new CSS classes"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,10 @@
 
     <title>Laravel</title>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
 
     @vite(['resources/js/app.js'])
 </head>
@@ -15,18 +19,20 @@
     <div id="app">
         <div class="container mx-auto">
 
-            <header>
+            <header class="py-6">
                 <h1>Laracasts</h1>
             </header>
 
 
-            <main>
-                <aside>
-                    <router-link to='/'>Home</router-link>
-                    <router-link to='/about'>About</router-link>
+            <main class="flex">
+                <aside class="w-1/5">
+                    <ul>
+                        <li><router-link to='/'>Home</router-link></li>
+                        <li><router-link to='/about'>About</router-link></li>
+                    </ul>
                 </aside>
 
-                <div class="primary">
+                <div class="primary flex-1">
                     <router-view></router-view>
                 </div>
             </main>
